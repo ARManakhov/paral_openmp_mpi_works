db49ca7a0229736bd88ad6fea71dd74655174dff
ARManakhov <sheih000@gmail.com>
2021-01-21 01:09:51 +0300

    mpi 5 done

--- ex/mpi/5/main.cpp
+++ ex/mpi/5/main.cpp
@@ -31,23 +31,20 @@ int main(int argc, char **argv) {
             printf("i = %d, a = %d, b=%d\n", i, a[i], b[i]);
         }
         for (int j = 0; j < n; ++j) {
-            send_arr[i * 2] = a[i];
-            send_arr[i * 2 + 1] = b[i];
+            send_arr[j * 2] = a[j];
+            send_arr[j * 2 + 1] = b[j];
         }
     }
 
     int m = n / size;
     int recv_arr[m * 2];
 
-    MPI_Scatter(a, m * 2, MPI_INT, recv_arr, m * 2, MPI_INT, root, MPI_COMM_WORLD);
+    MPI_Scatter(send_arr, m * 2, MPI_INT, recv_arr, m * 2, MPI_INT, root, MPI_COMM_WORLD);
 
     int scolar_comp_part = 0;
     for (i = 0; i < m; i++) {
         scolar_comp_part += recv_arr[i * 2] * recv_arr[i * 2 + 1];
-        printf("scolar_comp_part = %d\n", scolar_comp_part);
-
     }
-    printf("\n");
 
     int sum = 0;
     MPI_Reduce(&scolar_comp_part, &sum, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
